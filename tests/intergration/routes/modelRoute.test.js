bf35c343c00177b975af9207d1ecbd692f26c7f4
WKolaj <WKolaj@gmail.com>
2020-01-22 07:53:31 +0100

    Added integration tests for model route GET/:userId/:id

--- tests/intergration/routes/modelRoute.test.js
+++ tests/intergration/routes/modelRoute.test.js
@@ -97,5 +97,166 @@ describe("/api/models", () => {
 
       expect(response.body).toEqual(expectedPayload);
     });
+
+    it("should return 404 - if model doesnt exist", async () => {
+      modelId = mongoose.Types.ObjectId();
+
+      let response = await exec();
+
+      expect(response).toBeDefined();
+      expect(response.status).toEqual(404);
+      expect(response.text).toEqual("Model or user not found");
+    });
+
+    it("should return 404 - if user doesnt exist", async () => {
+      userId = mongoose.Types.ObjectId();
+
+      let response = await exec();
+
+      expect(response).toBeDefined();
+      expect(response.status).toEqual(404);
+      expect(response.text).toEqual("Model or user not found");
+    });
+
+    it("should return 404 - if user and model dont exist", async () => {
+      userId = mongoose.Types.ObjectId();
+      modelId = mongoose.Types.ObjectId();
+
+      let response = await exec();
+
+      expect(response).toBeDefined();
+      expect(response.status).toEqual(404);
+      expect(response.text).toEqual("Model or user not found");
+    });
+
+    it("should return 400 - if user Id is invalid", async () => {
+      userId = "abcd";
+
+      let response = await exec();
+
+      expect(response).toBeDefined();
+      expect(response.status).toEqual(404);
+      expect(response.text).toEqual("Invalid user id...");
+    });
+
+    it("should return 400 - if model Id is invalid", async () => {
+      modelId = "abcd";
+
+      let response = await exec();
+
+      expect(response).toBeDefined();
+      expect(response.status).toEqual(404);
+      expect(response.text).toEqual("Invalid id...");
+    });
+
+    it("should not return any user and return 401 if jwt has not been given", async () => {
+      jwt = undefined;
+
+      let response = await exec();
+
+      //#region CHECKING_RESPONSE
+
+      expect(response).toBeDefined();
+      expect(response.status).toEqual(401);
+      expect(response.text).toBeDefined();
+      expect(response.text).toContain("Access denied. No token provided");
+
+      //#endregion CHECKING_RESPONSE
+    });
+
+    it("should not return any user and return 403 if jwt of user has been given", async () => {
+      jwt = await testUser.generateJWT();
+
+      let response = await exec();
+
+      //#region CHECKING_RESPONSE
+
+      expect(response).toBeDefined();
+      expect(response.status).toEqual(403);
+      expect(response.text).toBeDefined();
+      expect(response.text).toContain("Access forbidden");
+
+      //#endregion CHECKING_RESPONSE
+    });
+
+    it("should not return any user and return 403 if jwt of useless (with permissions set to 0) user has been given", async () => {
+      jwt = await uselessUser.generateJWT();
+
+      let response = await exec();
+
+      //#region CHECKING_RESPONSE
+
+      expect(response).toBeDefined();
+      expect(response.status).toEqual(403);
+      expect(response.text).toBeDefined();
+      expect(response.text).toContain("Access forbidden");
+
+      //#endregion CHECKING_RESPONSE
+    });
+
+    it("should return users and return 200 with user payloadif jwt of adminAndUser user is given", async () => {
+      jwt = await testUserAndAdmin.generateJWT();
+
+      let response = await exec();
+
+      expect(response).toBeDefined();
+      expect(response.status).toEqual(200);
+
+      let expectedModel = modelsOfTestUser[1];
+
+      let expectedPayload = {
+        _id: expectedModel._id.toString(),
+        name: expectedModel.name,
+        user: expectedModel.user.toString()
+      };
+
+      expect(response.body).toEqual(expectedPayload);
+    });
+
+    it("should not return any user and return 400 if invalid jwt has been given", async () => {
+      jwt = "abcd1234";
+
+      let response = await exec();
+
+      //#region CHECKING_RESPONSE
+
+      expect(response).toBeDefined();
+      expect(response.status).toEqual(400);
+      expect(response.text).toBeDefined();
+      expect(response.text).toContain("Invalid token provided");
+
+      //#endregion CHECKING_RESPONSE
+
+      //#region CHECKING_DATABASE
+
+      //Only four users should be saved inside database - uselessUser, testAdmin, testUser, testAdminAndUser
+      let userCount = await User.countDocuments({});
+
+      expect(userCount).toEqual(4);
+
+      //#endregion CHECKING_DATABASE
+    });
+
+    it("should not return any user and return 400 if jwt from different private key was provided", async () => {
+      let fakeUserPayload = {
+        _id: testAdmin._id,
+        email: testAdmin.email,
+        name: testAdmin.name,
+        permissions: testAdmin.permissions
+      };
+
+      jwt = await jsonWebToken.sign(fakeUserPayload, "differentTestPrivateKey");
+
+      let response = await exec();
+
+      //#region CHECKING_RESPONSE
+
+      expect(response).toBeDefined();
+      expect(response.status).toEqual(400);
+      expect(response.text).toBeDefined();
+      expect(response.text).toContain("Invalid token provided");
+
+      //#endregion CHECKING_RESPONSE
+    });
   });
 });
