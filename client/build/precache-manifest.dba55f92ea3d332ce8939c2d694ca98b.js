eb12e6bac8625dcbff8fcc5775bedb330e9a69c1
WKolaj <WKolaj@gmail.com>
2020-02-17 22:28:00 +0100

    Fixed issue associated with invalid path

---
+++ client/build/precache-manifest.dba55f92ea3d332ce8939c2d694ca98b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c3f4bb4957d4d90656b2a18cc06a47",
+    "url": "sidiroar/index.html"
+  },
+  {
+    "revision": "b1bb292c670d2f8c86af",
+    "url": "sidiroar/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e6f0cb01ac90b33abb69",
+    "url": "sidiroar/static/js/2.02ef7f10.chunk.js"
+  },
+  {
+    "revision": "844fd56c98c605916b3d54f5cfc97fb3",
+    "url": "sidiroar/static/js/2.02ef7f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1bb292c670d2f8c86af",
+    "url": "sidiroar/static/js/main.3034bfab.chunk.js"
+  },
+  {
+    "revision": "b0f3122ba2cd0b81ecc5",
+    "url": "sidiroar/static/js/runtime-main.934669d7.js"
+  }
+]);
